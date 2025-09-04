34ac0b8a92e0ce4bb35e5316219ee3eb3f1ba07b
Anton Vasetenkov <antvaset@gmail.com>
2025-09-04 16:32:58 +1200

    testing cql-to-elm v4

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,10 @@ repositories {
 
 dependencies {
     // cql-to-elm-jvm also works
-    api("info.cqframework:cql-to-elm:4.0.0-SNAPSHOT")
+    implementation("info.cqframework:cql-to-elm:4.0.0-SNAPSHOT")
 
-    api("info.cqframework:quick:4.0.0-SNAPSHOT")
-    api("info.cqframework:ucum:4.0.0-SNAPSHOT")
+    implementation("info.cqframework:quick:4.0.0-SNAPSHOT")
+    implementation("info.cqframework:ucum:4.0.0-SNAPSHOT")
 }
 
 kotlin {
